4724731eb40aad4e611acfe9d3bb2a4eb4321942
Іван Довгаль <145868278+pypuc@users.noreply.github.com>
2024-10-13 16:51:53 +0300

    fixm1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-const 
+// 1 завднання
+
+const oneString = prompt("ведіть тест");
+const twoString = prompt("замок чи замок")
+
+if (oneString === "" || twoString === ""){
+    alert("Не всі поля заповнені")
+} else {
+    alert("Обидва поля заповнені")
+}
+
+// 1 завднання
