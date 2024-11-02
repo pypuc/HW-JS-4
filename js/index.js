dbeb3e67cdf569eae6427c30bb4dfd80fc2a71fa
Іван Довгаль <145868278+pypuc@users.noreply.github.com>
2024-11-02 16:06:40 +0200

    home work

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // 1 завднання
 
 const oneString = prompt("ведіть тест");
-const twoString = prompt("замок чи замок")
+const twoString = prompt("ведіть тест")
 
 if (oneString === "" || twoString === ""){
     alert("Не всі поля заповнені")
@@ -9,4 +9,51 @@ if (oneString === "" || twoString === ""){
     alert("Обидва поля заповнені")
 }
 
-// 1 завднання
+// 2 завднання
+
+const stingNumberone = Number(prompt("ведіть число"));
+const stingNumbertwo = Number(prompt("ведіть число"));
+
+if (stingNumberone + stingNumbertwo < 10){
+    alert("Сума менша або дорівнює 10")
+} else {
+    alert("Сума більша за 10")
+}
+
+// 3 завдання
+
+const string = prompt("як поіншому можна назвати мову програмування ECMAScript");
+
+if (string === "JavaScript") {
+    alert("Текст містить слово JavaScript")
+} else {
+    alert( "Текст не містить слово JavaScript")
+}
+
+// 4 завдання
+
+const stingNumber = prompt("ведіть число")
+
+if (stingNumber > 10 && stingNumber < 20){
+    alert("Число входить в діапазон від 10 до 20")
+} else {
+    alert("Число не входить в діапазон від 10 до 20")
+}
+
+// 5 завдання
+
+const email = prompt("ведіть свій емаіл")
+
+if (email === '@' && email === "."){
+    alert('вірно')
+} else {
+    alert("не вірно")
+}
+
+const password = prompt("ведіть пароль (jsfd123)")
+
+if (password === "jsfd123") {
+    alert("перейдіть сюди https://rozetka.com.ua/")
+} else {
+    alert("помилка")
+}
